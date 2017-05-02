5448a6dd60e77b9f247255a8ffb7b51d018de1b5
Andrey Pridatchenko <andrey.pridatchenko@rcntec.com>
2017-05-02 13:37:23 +0300

    Add mail notify

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -5,4 +5,17 @@ class Ticket < ApplicationRecord
   belongs_to :last_station, class_name: 'RailwayStation', foreign_key: :last_station_id
 
   validates :user_passport, :user_name, presence: true
+
+  after_create :send_buy_email
+  after_destroy :send_delete_email
+
+  private
+
+  def send_buy_email
+    TicketsMailer.deliver_buy_ticket(self.user, self)
+  end
+
+  def send_delete_email
+    TicketsMailer.deliver_delete_ticket(self.user, self)
+  end
 end
